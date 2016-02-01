35d1473aaacf2d75b042e72f985bef60643411cb
JohnSmith <johnsmith1st@live.com>
2016-02-01 11:43:25 +0800

    [add] bootstrap, react bootstrap

--- src/component.jsx
+++ src/component.jsx
@@ -1,10 +1,17 @@
 'use strict';
 
+import 'bootstrap/dist/css/bootstrap.css'
 import './component.css';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class Hello extends React.Component {
   render() {
-    return <h1 className="red">Hello World 101</h1>;
+    return (
+      <div>
+        <h1 className="red">Hello World 101</h1>
+        <Button bsStyle="primary">Click ME !!!</Button>
+      </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,21 @@ var path = require('path');
 var node_modules_dir = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules_dir, 'react/dist/react.min.js');
 
-module.exports = {
+var deps = [
+  'react/dist/react.min.js',
+  'react-dom/dist/react-dom.min.js',
+  'react-router/dist/react-router.min.js',
+  'react-bootstrap/dist/react-bootstrap.min.js'
+];
+
+var config = {
   entry: [
     'webpack/hot/dev-server',
     'webpack-dev-server/client?http://localhost:8080',
     path.resolve(__dirname, 'src/main.js')
   ],
   resolve: {
-    alias: {
-      'react': pathToReact
-    }
+    alias: {}
   },
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -31,8 +36,36 @@ module.exports = {
       {
         test: /\.css$/, // Only .css files
         loader: 'style!css' // Run both style-loader and css-loader
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff2"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
       }
     ],
-    noParse: [pathToReact]
+    noParse: []
   }
 };
+
+deps.forEach(dep => {
+  var depPath = path.resolve(node_modules_dir, dep);
+  config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+  config.module.noParse.push(depPath);
+});
+
+module.exports = config;
