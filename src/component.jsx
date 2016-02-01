06a1e579d5748ba5e336134bbd89984467de2a7a
JohnSmith <johnsmith1st@live.com>
2016-02-01 11:55:46 +0800

    [add] a modal dialog demo

--- src/component.jsx
+++ src/component.jsx
@@ -3,14 +3,40 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './component.css';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 export default class Hello extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { showModal: false };
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <div>
         <h1 className="red">Hello World 101</h1>
-        <Button bsStyle="primary">Click ME !!!</Button>
+        <Button bsStyle="primary" onClick={this.open.bind(this)}>Click ME !!!</Button>
+
+        <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Hello Modal</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>It works !!!</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close.bind(this)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
